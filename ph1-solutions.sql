de650d1bc52bb8958c2437c64b8ebbd1c63eb395
amanda <amanda@zipcodes-MacBook-Pro.local>
2020-12-06 22:02:50 -0500

    Through 6 without 9 or 4d

--- ph1-solutions.sql
+++ ph1-solutions.sql
@@ -4,3 +4,49 @@
 USE sakila;
 
 -- Your solutions...
+--1
+select first_name, last_name from actor where first_name = 'Scarlett';
+
+--2
+select first_name, last_name from actor where last_name = 'Johansson';
+
+--3
+select count(distinct(last_name)) distinct_last_name from actor;
+
+--4
+select last_name, count(*) from actor
+group by last_name having count(*) = 1;
+
+--5
+select last_name, count(*) from actor
+group by last_name having count(*) > 1;
+
+--6
+select a.first_name, a.last_name, count(fa.film_id) film_max
+from film_actor fa join actor a on fa.actor_id = a.actor_id
+group by fa.actor_id order by film_max desc limit 1;
+
+--7
+select f.title, i.store_id from inventory i
+right join film f on i.film_id = f.film_id
+where f.title = 'Academy Dinosaur';
+
+--8
+select * from customer where first_name = 'Mary' and last_name = 'Smith';
+select film_id, title from film where title = 'Academy Dinosaur';
+select staff_id, store_id from staff where first_name = 'Mike' and last_name = 'Hillyer';
+insert into rental (customer_id, inventory_id, staff_id, rental_date) values (1, 1, 1, now());
+
+--9
+
+--10
+select avg(length) as average_run_time from film_list;
+
+--11
+select category, avg(length) as average_run_time
+from film_list group by category;
+
+--12
+--The last_update column is common between the two tables, but its not likely that
+--both the film_id (the other matching column) and the last_update column will both
+--match between the two tables because date time is very specific.
--- ph2-solutions.sql
+++ ph2-solutions.sql
@@ -4,3 +4,84 @@
 USE sakila;
 
 -- Your solutions...
+--1a
+select first_name, last_name from actor;
+
+--1b
+select concat(upper(first_name), ' ',upper(last_name)) 'Actor Name' from actor;
+
+--2a
+select actor_id, first_name, last_name from actor where first_name = 'Joe';
+
+--2b
+select first_name, last_name from actor where last_name like '%gen%';
+
+--2c
+select last_name, first_name from actor where last_name like '%li%' order by last_name, first_name;
+
+--2d
+select country_id, country from country where country in ('Afghanistan','Bangladesh', 'China');
+
+--3a
+alter table actor add middle_name varchar(45) after first_name;
+
+--3b
+alter table actor modify column middle_name blob;
+
+--3c
+alter table actor drop column middle_name;
+
+--4a
+select last_name, count(*)
+from actor group by last_name
+order by count(*) desc;
+
+--4b
+select last_name, count(*)
+from actor group by last_name
+having count(*) > 1 order by count(*) desc;
+
+--4c
+update actor set first_name = 'HARPO'
+where last_name = 'Williams' and first_name = 'Groucho';
+
+--4d
+
+
+
+
+
+--5a
+show create table address;
+
+--6a
+select concat(s.first_name, ' ', s.last_name), a.address
+from staff s
+left join address a on s.address_id = a.address_id;
+
+--6b
+select concat(s.first_name, ' ', s.last_name), count(*) transactions
+from payment p right join staff s on p.staff_id = s.staff_id
+where p.payment_date like '%2005-08%' group by p.staff_id;
+
+--6c
+select f.title, count(*) num_actors
+from film_actor fa inner join film f on fa.film_id = f.film_id
+group by fa.film_id order by count(*) desc;
+
+--6d
+select film_id, title from film where title = 'Hunchback Impossible';
+select film_id, count(*) copies from inventory where film_id = 439;
+--or
+select f.title, count(*) copies
+from inventory i join film f on i.film_id = f.film_id
+where f.title = 'Hunchback Impossible';
+
+--6e
+select c.last_name, c.first_name, sum(p.amount) total_payment
+from payment p join customer c on p.customer_id = c.customer_id
+group by c.last_name, c.first_name order by c.last_name;
+
+--7a
+
+
